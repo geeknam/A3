f31a14f6fce7ae289af5be61f95fe35372adea5e
Nam Ngo <emoinrp@gmail.com>
2013-11-14 18:43:49 +1100

    Create manifest view.

--- allocate3/static/js/main.js
+++ allocate3/static/js/main.js
@@ -1,26 +1,9 @@
-$(document)
-  .ready(function() {
-
-    $('.filter.menu .item')
-      .tab()
-    ;
-
-    $('.ui.rating')
-      .rating({
-        clearable: true
-      })
-    ;
-
-    $('.ui.sidebar')
-      .sidebar('attach events', '.launch.button')
-    ;
-
-    $('.ui.dropdown')
-      .dropdown()
-    ;
-
-  })
-;
+$(document).ready(function() {
+    $('.filter.menu .item').tab();
+    $('.ui.rating').rating({clearable: true});
+    $('.ui.sidebar').sidebar('attach events', '.launch.button');
+    $('.ui.dropdown').dropdown();
+});
 
 var allocate = angular.module('AllocateApp', []);
 
